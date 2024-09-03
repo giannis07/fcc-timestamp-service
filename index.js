c8e1abbe4cae5ff9c6af33ac7501ce197b252b7a
Giannis Mousouleas <yannismousouleas@gmail.com>
2024-09-03 15:44:50 +0000

    Index.js

--- index.js
+++ index.js
@@ -24,6 +24,35 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+// Timestamp API endpoint
+app.get("/api/:date?", function (req, res) {
+  let date = req.params.date;
+
+  // If no date is provided, use the current date
+  if (!date) {
+    date = new Date();
+  } else {
+    // Check if the date is a valid Unix timestamp
+    if (/^\d+$/.test(date)) {
+      date = parseInt(date);
+    }
+    date = new Date(date);
+  }
+
+  // If  date is invalid, return message Invalid
+  if (date.toString() === "Invalid Date") {
+    return res.json({ error: "Invalid Date" });
+  }
+
+  // Return the Unix timestamp and UTC date string
+  res.json({
+    unix: date.getTime(),
+    utc: date.toUTCString()
+  });
+
+}); 
+
+
 
 
 // Listen on port set in environment variable or default to 3000
